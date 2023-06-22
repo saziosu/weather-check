34d5871d0970deb67a42edc2099dd4ee21488f5c
saziosu <>
2023-06-22 19:20:02 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import requests
+import datetime
+
+API_CRED = open("credentials", "r").read()
+LOCATION_URL = "http://api.openweathermap.org/geo/1.0/direct?q="
+""" 
+full url for location is http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit={limit}&appid={API key}
+"""
+CITY_URL = "https://api.openweathermap.org/data/2.5/weather?"
+
+"""
+full url for current weather is https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+"""
+
