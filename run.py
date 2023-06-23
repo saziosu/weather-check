c95a35965436a19fc596b22261dd71db3e4e5ba9
saziosu <>
2023-06-23 18:07:38 +0000

    Add city_check function to confirm user's desired location via input and return latitude and longitude data

--- run.py
+++ run.py
@@ -1,14 +1,36 @@
 import requests
 import datetime
 
-API_CRED = open("credentials", "r").read()
-LOCATION_URL = "http://api.openweathermap.org/geo/1.0/direct?q="
+API = open("credentials", "r").read()
+LOCATION_URL_BASE = "http://api.openweathermap.org/geo/1.0/direct?q="
 """ 
-full url for location is http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit={limit}&appid={API key}
+full url for location is
+http://api.openweathermap.org/geo/1.0/direct?q={cityname}&limit={limit}&appid={APIkey}
 """
-CITY_URL = "https://api.openweathermap.org/data/2.5/weather?"
+CITY_URL_BASE = "https://api.openweathermap.org/data/2.5/weather?"
 
 """
-full url for current weather is https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+full url for current weather is
+https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={APIkey}
 """
 
+
+def city_check():
+    '''
+    Checks the validity of the User's input and pulls the
+    latitude and longitude from their city input to return
+    '''
+    city = input("Please enter your city: \n")
+    location = requests.get(f"{LOCATION_URL_BASE}{city}&limit=1&appid={API}")
+    location_status = location.status_code
+    location_detail = location.json()[0]
+    latitude = location_detail['lat']
+    longitude = location_detail['lon']
+    if location_status == 200:
+        print(f"Great! Checking weather for {city}...")
+    else:
+        print("Error found, please run again")
+    return latitude, longitude
+
+
+main()
